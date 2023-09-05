334dfdb73732aa438afabd839d103b85f02a659d
sasikumar1591 <113574444+sasikumar1591@users.noreply.github.com>
2023-09-05 09:09:16 +0530

    Update 24.high booster mask.py

--- 24.high booster mask.py
+++ 24.high booster mask.py
@@ -1,26 +1,7 @@
-import cv2
-import numpy as np
-
-# Load the image in grayscale
-img = cv2.imread('C:/Users/ELCOT/Downloads/download.jpg', cv2.IMREAD_GRAYSCALE)
-
-# Apply a Gaussian filter to the image
-blur = cv2.GaussianBlur(img, (5,5), 0)
-
-# Calculate the high-pass filter
-high_pass = img - blur
-
-# Define the constant A for sharpening
-A = 2.0
-
-# Multiply the high-pass filter by the constant A
-high_boost = A * high_pass
-
-# Add the high-pass filter to the original image
-sharpened = img + high_boost
-
-# Display the original and sharpened images
-cv2.imshow('Original', img)
-cv2.imshow('Sharpened', sharpened)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+import pandas as pd
+course={ 'sub':['python','cplusplus','python','c','cpluscplus','fod','fod','c']
+         ,'coursecount':[34,40,32,40,35,40,34,32]}
+data=pd.DataFrame(course)
+print(data)
+data1=data['coursecount'].value_counts()
+print("frequency of the given data :",data1)
